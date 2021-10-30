795df4ef90cc58f39803031d12a10afa61b667f9
seungjuoh <sjoh0704@daum.net>
2021-10-30 20:35:45 +0900

    fix error

--- function/checkface.py
+++ function/checkface.py
@@ -3,20 +3,25 @@ from keras.models import load_model
 from PIL import Image, ImageOps
 import numpy as np
 import urllib.request
+from io import BytesIO
+
 def handler(event, context):
     print("event occurs")
-    # download image
     try:
-        url = event['data']
-        urllib.request.urlretrieve(url, "test.jpg")
+        # url = event['data']['imageURL']
+        url = "https://hanbucket-test.s3.ap-northeast-2.amazonaws.com/img.png"
+        print(url)
+        req = urllib.request.Request(url, headers = {"User-Agent" : "Mozilla/5.0"})
+        res = urllib.request.urlopen(req).read()
+        image = Image.open(BytesIO(res))
+      
     except Exception as e:
         print(e)
-        return 
+        return 'fail'
 
     # keras 
     model = load_model('./model/keras_model.h5')
     data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
-    image = Image.open('./test.jpg')
     size = (224, 224)
     image = ImageOps.fit(image, size, Image.ANTIALIAS)
     image_array = np.asarray(image)
@@ -27,4 +32,6 @@ def handler(event, context):
     #result
     print("사람:", float(prediction[0][0]))
     print("손바닥:", float(prediction[0][1]))
-# handler(1, 1)
+    return 'ok'
+handler(1, 1)
+
