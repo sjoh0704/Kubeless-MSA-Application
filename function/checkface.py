35926dd23f17394ad3d4803dca0578d3048c479d
seungjuoh <sjoh0704@daum.net>
2021-10-30 21:41:19 +0900

    change model

--- function/checkface.py
+++ function/checkface.py
@@ -8,30 +8,32 @@ from io import BytesIO
 def handler(event, context):
     print("event occurs")
     try:
-        url = event['data']['imageURL']
+        url = event['data']['imageUzRL']
         # url = "https://hanbucket-test.s3.ap-northeast-2.amazonaws.com/img.png"
-        print(url)
+        # print(url)
         req = urllib.request.Request(url, headers = {"User-Agent" : "Mozilla/5.0"})
         res = urllib.request.urlopen(req).read()
         image = Image.open(BytesIO(res))
-      
+
+         # keras 
+        model = load_model('./model/model.savedmodel')
+        data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+        size = (224, 224)
+        image = ImageOps.fit(image, size, Image.ANTIALIAS)
+        image_array = np.asarray(image)
+        normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+        data[0] = normalized_image_array
+        prediction = model.predict(data)
+
+        #result
+        print("사람:", float(prediction[0][0]))
+        print("손바닥:", float(prediction[0][1]))
+        return 'ok'
+        
     except Exception as e:
         print(e)
         return 'fail'
 
-    # keras 
-    model = load_model('./model/keras_model.h5')
-    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
-    size = (224, 224)
-    image = ImageOps.fit(image, size, Image.ANTIALIAS)
-    image_array = np.asarray(image)
-    normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
-    data[0] = normalized_image_array
-    prediction = model.predict(data)
-
-    #result
-    print("사람:", float(prediction[0][0]))
-    print("손바닥:", float(prediction[0][1]))
-    return 'ok'
-handler(1, 1)
+   
+# handler(1, 1)
 
