bd2157f9441a34ed4bebae4633e776b645b024f8
seungjuoh <sjoh0704@daum.net>
2021-10-30 10:53:42 +0900

    add s3 bucket image upload

--- front-app/src/components/UploadImage.js
+++ front-app/src/components/UploadImage.js
@@ -1,19 +1,36 @@
 import { useState } from "react";
 import '../assets/css/ImageUpload.css'
+import { S3Config, UploadS3 } from "./UploadS3";
+import axios from 'axios'
+
 const UploadImage = () => {
     const [state, setState] = useState(null);
     console.log(state);
 
     const onSubmit =(event)=>{
-        console.log("클릭")
+        const body = {
+            imageURL: `https://${S3Config.bucketName}.s3.${S3Config.region}.amazonaws.com/${state.name}` 
+        };
+        console.log(body)
+
+
+        UploadS3([state]);
+
+        // axios
+        //     .post("#", body)
+        //     .then((response) => {
+        //     })
+        //     .catch((e) => {
+
+        //     });
     }
 
     return (
         <div>
-            <input type="file" class="custom-file-input" id="input" accept="image/*" onChange={(event) => setState(URL.createObjectURL(event.target.files[0]))} value="" />
+            <input type="file" class="custom-file-input" id="input" accept="image/*" onChange={(event) => setState(event.target.files[0])} value="" />
 
             {state && <button onClick={(event) => setState(null)}>Remove Image</button>}
-            <img id="output" style={{ width: "50%" }} src={state} />
+            {state && <img id="output" style={{ width: "50%" }} src={URL.createObjectURL(state)} />}
             {state && <button onClick={(event) => onSubmit(event)}>제출하기</button>}
         </div>
     );
