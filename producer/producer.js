47214324b63fa6057990d54333f06e8600e2aa5e
seung <sjoh0704@daum.net>
2021-11-10 20:51:31 +0900

    add pub to producer.js

--- producer/producer.js
+++ producer/producer.js
@@ -1,3 +1,26 @@
+const { Kafka } = require("kafkajs");
+
+const kafka = new Kafka({
+  clientId: "my-app",
+  brokers: ["localhost:9092"],
+});
+
+const producer = kafka.producer();
+
+const run = async (data) => {
+  // Producing
+  await producer.connect();
+  console.log(1)
+  await producer.send({
+    topic: "topic1",
+    messages: [{ value: data }],
+  });
+  console.log(2)
+  process.exit()
+};
+
+
+
 module.exports = {
     producer: function async(event, context) {
         const axios = require("axios");
@@ -6,10 +29,11 @@ module.exports = {
             
             // const data = event["data"];
             const data = { userId: "1234123dsfadf", a: 0.1, b: 0.2 };
-            axios.post("http://checkface.default.svc:8080", data).then((res) => {
-                console.log(res);
-                return "ok";
-            });
+            run(data).catch(console.error);
+            // axios.post("http://checkface.default.svc:8080", data).then((res) => {
+            //     console.log(res);
+            //     return "ok";
+            // });
         } catch (error) {
             console.log(error);
             return "fail";
