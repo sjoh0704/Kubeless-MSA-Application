8b235e88031ca68134f81ec5c9f6e75fd0a674c6
seung <sjoh0704@daum.net>
2021-11-13 01:07:22 +0900

    test

--- producer/producer.js
+++ producer/producer.js
@@ -1,55 +1,51 @@
+const { Kafka } = require("kafkajs");
 
+const kafka = new Kafka({
+  clientId: "my-app",
+  brokers: ["kafka.kubeless.svc:9092"],
+  // brokers: ["localhost:9092"]
+});
 
+const producer = kafka.producer();
 
-
-// const run = async (data) => {
-//   // Producing
-//   console.log("run!!");
-//   await producer.connect();
-//   await producer.send({
-//     topic: "test-topic",
-//     messages: [{ value: data }],
-//   });
-//   process.exit()
-// };
+const run = async (data) => {
+  // Producing
+  console.log("run!!");
+  await producer.connect();
+  await producer.send({
+    topic: "test-topic",
+    messages: [{ value: null, data: data }],
+  });
+  process.exit()
+};
 
 
 
 module.exports = {
     producer: function async(event, context) {
-      console.log("event occurs");
-      const { Kafka } = require("kafkajs");
+      console.log("event occurs")
 
-      const kafka = new Kafka({
-        clientId: "my-app",
-        brokers: ["kafka.kubeless.svc:9092"],
-      });
-      const producer = kafka.producer();
         // const axios = require("axios");
         console.log(event);
         try {
             
             const data = event["data"];
             // const data = { userId: "1234123dsfadf", a: 0.1, b: 0.2 };
+            // const data = "data"
             console.log(data);
-            // run(data).catch(console.error);
+            run(data).catch((e)=>{
+              console.log(e);
+              process.exit()
+            }); 
             // axios.post("http://checkface.default.svc:8080", data).then((res) => {
             //     console.log(res);
             //     return "ok";
             // });
-            console.log("run!!");
-            
-            await producer.connect();
-            console.log("connect")
-            await producer.send({
-              topic: "test-topic",
-              messages: [{ value: data }],
-            });
-            process.exit()
         } catch (error) {
             console.log(error);
             return "fail";
         }
 
+        return "fail";
     },
 };
