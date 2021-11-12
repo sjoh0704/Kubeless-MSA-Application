fa293aec3bae9227b2a26fc915d9c1b3e8d8038f
seung <sjoh0704@daum.net>
2021-11-12 21:51:40 +0900

    test

--- producer/producer.js
+++ producer/producer.js
@@ -1,29 +1,30 @@
-const { Kafka } = require("kafkajs");
 
-const kafka = new Kafka({
-  clientId: "my-app",
-  brokers: ["kafka.kubeless.svc:9092"],
-});
 
-const producer = kafka.producer();
 
-const run = async (data) => {
-  // Producing
-  console.log("run!!");
-  await producer.connect();
-  await producer.send({
-    topic: "test-topic",
-    messages: [{ value: data }],
-  });
-  process.exit()
-};
+
+// const run = async (data) => {
+//   // Producing
+//   console.log("run!!");
+//   await producer.connect();
+//   await producer.send({
+//     topic: "test-topic",
+//     messages: [{ value: data }],
+//   });
+//   process.exit()
+// };
 
 
 
 module.exports = {
     producer: function async(event, context) {
       console.log("event occurs");
+      const { Kafka } = require("kafkajs");
 
+      const kafka = new Kafka({
+        clientId: "my-app",
+        brokers: ["kafka.kubeless.svc:9092"],
+      });
+      const producer = kafka.producer();
         // const axios = require("axios");
         console.log(event);
         try {
@@ -31,16 +32,24 @@ module.exports = {
             const data = event["data"];
             // const data = { userId: "1234123dsfadf", a: 0.1, b: 0.2 };
             console.log(data);
-            run(data).catch(console.error);
+            // run(data).catch(console.error);
             // axios.post("http://checkface.default.svc:8080", data).then((res) => {
             //     console.log(res);
             //     return "ok";
             // });
+            console.log("run!!");
+            
+            await producer.connect();
+            console.log("connect")
+            await producer.send({
+              topic: "test-topic",
+              messages: [{ value: data }],
+            });
+            process.exit()
         } catch (error) {
             console.log(error);
             return "fail";
         }
 
-        return "fail";
     },
 };
